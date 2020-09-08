2c4ea7ca1dee2bb13661685755f954bff480decd
Gerecske <70981967+Gerecske@users.noreply.github.com>
2020-09-09 00:01:46 +0200

    Basic movement and musicbox script

---
+++ Move.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public InputAction wasd;
+	public InputAction mouse;
+
+	public float mSens = 100f;
+	public Transform playerBody;
+
+	public float speed = 12f;
+	public float gravity = -9.81f;
+
+	Vector3 velocity;
+
+	float xRotation = 0f;
+
+	public CharacterController controller;
+	void OnEnable()
+	{
+        wasd.Enable();
+		mouse.Enable();
+	}
+
+	void OnDisable()
+	{
+        wasd.Disable();
+		mouse.Disable();
+	}
+
+	void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update()
+    {
+        Vector2 inputVector = wasd.ReadValue<Vector2>();
+		Vector3 finVector = new Vector3();
+		finVector.x = inputVector.x * speed * -1f;
+		finVector.z = inputVector.y * speed;
+
+		Vector3 move = playerBody.right * finVector.x + playerBody.forward * finVector.z;
+
+		controller.Move(move * speed * Time.deltaTime);
+
+		velocity.y += gravity * Time.deltaTime;
+
+		controller.Move(velocity * Time.deltaTime);
+
+		Vector2 inputMVector = mouse.ReadValue<Vector2>();
+
+		float mouseX = inputMVector.x * mSens * Time.deltaTime;
+		float mouseY = inputMVector.y * mSens * Time.deltaTime;
+
+		xRotation -= mouseY;
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		playerBody.Rotate(Vector3.up * mouseX);
+	}
+}
---
+++ MusicBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiomenager : MonoBehaviour
+{
+    public AudioClip fIt, music;
+    public AudioSource audioScr;
+    bool fou = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioScr.PlayOneShot(music);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0f, 0.5f, 0f);
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+    }
+
+    public void QuitGame()
+	{
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+        Debug.Log(collision.collider.name);
+		if (collision.collider.name == "FPPlayer")
+		{
+			if (fou == false)
+			{
+                audioScr.PlayOneShot(fIt);
+                Invoke("QuitGame", 0.5f);
+                fou = true;
+            }
+        }
+    }
+}
